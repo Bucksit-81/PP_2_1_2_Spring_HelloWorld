7a08eccc6a4c4b7a485742bf92fbec4269bd2b26
Bucksit-81 <5229290@gmail.com>
2023-04-17 03:52:35 +0300

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,15 +3,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
-        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) context.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) context.getBean("helloworld");
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (bean == bean2));
 
-        Cat cat1 = (Cat) applicationContext.getBean("cat");
-        Cat cat2 = (Cat) applicationContext.getBean("cat");
-
-        System.out.println("Первая пара - " + (bean1 == bean2));
-        System.out.println("Вторая пара - " + (cat1 == cat2));
+        Cat Cat1 = (Cat) context.getBean("cat");
+        Cat Cat2 = (Cat) context.getBean("cat");
+        System.out.println(Cat1.getMessage());
+        System.out.println("Переменные ссылаются на один и тот же объект? " + (Cat1 == Cat2));
     }
 }
